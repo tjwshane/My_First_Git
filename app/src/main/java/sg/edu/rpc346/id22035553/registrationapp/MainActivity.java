d6020297d722011df39d2c3400c0829a1aca67d0
TAN JIN WEI <22035553@myrp.edu.sg>
2023-05-02 16:22:42 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rpc346/id22035553/registrationapp/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rpc346.id22035553.registrationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
